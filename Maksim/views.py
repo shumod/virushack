121c7f27dee28aa8e036bc523a3c3322b0e11460
Maksim <yellow444@gmail.com>
2020-05-04 12:45:09 +0300

    шаблон и грабер картинки

---
+++ Maksim/views.py
@@ -0,0 +1,21 @@
+"""
+Routes and views for the flask application.
+"""
+
+from flask import render_template
+from VirusHackCv import app
+import cv2
+import base64
+from flask import Flask
+
+
+@app.route('/')
+@app.route('/video_feed')
+def cam():
+    capture = cv2.VideoCapture(0)
+    frame = capture.read()[1]
+    cnt = cv2.imencode('.png',frame)[1]
+    b64 = base64.b64encode(cnt).decode()
+    b64_src = 'data:image/png;base64,'
+    img_src = b64_src +  b64
+    return render_template("outputs.html",user_image=  img_src )
